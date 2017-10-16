43939744de0a0368945953069a0abdd724827180
Luke Horvat <lukehorvat@gmail.com>
2017-10-16 20:24:11 +1100

    Enable analytics

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -13,3 +13,11 @@ game.init().then(() => {
     game.start();
   }, false);
 });
+
+// Google Analytics.
+(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,"script","https://www.google-analytics.com/analytics.js","ga");
+ga("create", "UA-24505142-8", "auto");
+ga("send", "pageview");
